93a9b53f197f74ee583baa0e5d088a10476be72d
Marlena Tsang <Marlena345@gmail.com>
2021-12-20 22:46:29 -0500

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Messages(){
+  return <div>
+    <h1>This is the message component!</h1>
+  </div>
+}
+
+export default Messages;
---
+++ src/Signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//define the sign up  component
+function Signup(){
+  return <div>
+    <h1>This is my sign up component!</h1>
+    </div>
+}
+
+export default Signup;
